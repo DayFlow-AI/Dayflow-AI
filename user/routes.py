303ba1375c1e4f5fda846b14bc8d56ef368fd226
Rostislav Nefedov <nefedov.r75@gmail.com>
2026-08-07 02:02:00 +0300

    complete jwt-auth

--- user/routes.py
+++ user/routes.py
@@ -1,31 +1,69 @@
-from fastapi import HTTPException, Response, APIRouter, Depends
-from sqlalchemy import select
-from db import SessionDep
-from user.models import UsersORM
-from user.schemas import UserSchema, UserLoginSchema
-from authx import AuthX, AuthXConfig
+from fastapi import HTTPException, Response, APIRouter, status, Depends
+from fastapi_jwt import JwtAuthorizationCredentials
+from sqlalchemy import select, exists, or_, delete
+from db_conf.db import SessionDep
+from user.models import UsersORM, UsersJWTStorageORM
+from user.schemas import UserRegisterSchema, UserLoginSchema
+from user.utils import conf, hashing, password_hash, gen_jwt, access_security
+
 router = APIRouter(prefix="/user", tags=["user"])
 
-@router.get("/get/")
-async def users(data: UserSchema, session: SessionDep):
-    query = select(UsersORM)
-    result = await session.execute(query)
-    return result.scalars().all()
+
+@router.post("/register")
+async def register(user: UserRegisterSchema, db: SessionDep) -> Response:
+    query = select(exists().where(
+        or_(
+            UsersORM.email == user.email, UsersORM.username == user.username
+        )
+    ))
+    exists_result = await db.execute(query)
+    if not exists_result.scalar():
+        new_user = await hashing(user, db)
+        jwt = await gen_jwt(new_user, db)
+        response = Response(status_code=status.HTTP_201_CREATED)
+        response.set_cookie(**conf(jwt))
+        return response
+    raise HTTPException(status_code=401, detail="Email or username already registered")
 
 
-config = AuthXConfig()
-security = AuthX(config=config)
+@router.post("/login")
+async def login_user(login: UserLoginSchema, db: SessionDep) -> Response:
+    query = select(UsersORM).where(
+        or_(
+            UsersORM.email == login.login, UsersORM.username == login.login
+        )
+    )
+    exists_login = await db.execute(query)
+    exists_login = exists_login.scalar()
+    if exists_login:
+        access = password_hash.verify(login.password, exists_login.password)
+        if access:
+            jwt = await gen_jwt(exists_login, db)
+            response = Response(status_code=status.HTTP_201_CREATED)
+            response.set_cookie(**conf(jwt))
+            return response
+        raise HTTPException(status_code=401, detail="Incorrect password")
+    raise HTTPException(status_code=400, detail="Incorrect email/username or password")
 
 
-@router.post("/login/")
-async def login(credentials: UserLoginSchema, response: Response):
-    if credentials.email and credentials.password:
-        token = security.create_access_token(uid='12321')
-        response.set_cookie(config.JWT_ACCESS_COOKIE_NAME, token)
-        return {"access_token": token}
-    raise HTTPException(status_code=401, detail="Incorrect username or password")
+@router.post("/logout")
+async def logout(db: SessionDep, credentials: JwtAuthorizationCredentials = Depends(access_security)) -> Response:
+    await db.execute(
+        delete(UsersJWTStorageORM).where(UsersJWTStorageORM.jwt_token == credentials.jti)
+    )
+    await db.commit()
+    response = Response(status_code=status.HTTP_204_NO_CONTENT)
+    access_security.unset_access_cookie(response)
+    return response
 
 
-@router.get("/protected/", dependencies=[Depends(security.access_token_required)])
-async def protected():
-    return {"message": "Hello World"}
+@router.post("/logout_from_all_devices")
+async def logout_from_all_devices(db: SessionDep, credentials: JwtAuthorizationCredentials = Depends(access_security)) -> Response:
+    user_id = int(credentials.subject["uid"])
+    await db.execute(
+        delete(UsersJWTStorageORM).where(UsersJWTStorageORM.user_id == user_id)
+    )
+    await db.commit()
+    response = Response(status_code=status.HTTP_204_NO_CONTENT)
+    access_security.unset_access_cookie(response)
+    return response
