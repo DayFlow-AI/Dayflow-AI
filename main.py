8c7eb1c8c246496c5d1f6c23acd1e5d37f78cf5b
Rostislav Nefedov <nefedov.r75@gmail.com>
2026-08-07 02:02:36 +0300

    add cors and remove simple db api

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import uvicorn
 from fastapi import FastAPI
-from db import engine, Base
+from fastapi.middleware.cors import CORSMiddleware
 from user.routes import router as user_router
 
-app = FastAPI()
+app = FastAPI(debug=True, title="DayFlow API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://172.30.88.252:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(user_router)
-
-@app.get("/setup_database")
-async def setup_database():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
-
 
 if __name__ == "__main__":
     uvicorn.run(app, host="172.30.88.250", port=8000)
