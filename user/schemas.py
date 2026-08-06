0b264a1f236fac6ac51e17f7cc4dc01e3f151c1c
Rostislav Nefedov <nefedov.r75@gmail.com>
2026-08-07 02:04:25 +0300

    add register schema

--- user/schemas.py
+++ user/schemas.py
@@ -1,12 +1,20 @@
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import EmailStr, Field, ConfigDict
+from db_conf.base_schema import CustomSchema, CustomOutSchema
 
 
-class UserSchema(BaseModel):
+class UserPublicSchema(CustomOutSchema):
+    email: EmailStr
+    username: str = Field(max_length=100)
+
+
+class UserRegisterSchema(CustomSchema):
+    model_config = ConfigDict(from_attributes=True)
+
     email: EmailStr
     username: str = Field(max_length=100)
     password: str = Field(max_length=100)
 
 
-class UserLoginSchema(BaseModel):
-    email: EmailStr
+class UserLoginSchema(CustomSchema):
+    login: str = Field(max_length=100)
     password: str = Field(max_length=100)
