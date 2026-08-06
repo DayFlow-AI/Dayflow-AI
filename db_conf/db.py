dc1672aee7eee9e01d903acd5ea3bb787105fe50
Rostislav Nefedov <nefedov.r75@gmail.com>
2026-08-07 02:03:24 +0300

    relocate db and schema confs

--- db_conf/db.py
+++ db_conf/db.py
@@ -1,9 +1,11 @@
 from dotenv import load_dotenv
 from os import getenv as gv
 from urllib.parse import quote_plus
+from datetime import datetime
 from fastapi import Depends
+from sqlalchemy import DateTime, func
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from typing import Annotated
 
 load_dotenv()
@@ -15,7 +17,8 @@ db_schema = gv("DB_SCHEMA")
 if not all([db_user, db_pass, db_schema]):
     raise RuntimeError("environment variables not set check env file")
 
-engine = create_async_engine(f"postgresql+psycopg://{db_user}:{db_pass}@localhost:5432/{db_schema}")
+db_url = f"postgresql+psycopg://{db_user}:{db_pass}@localhost:5432/{db_schema}"
+engine = create_async_engine(db_url)
 
 new_session = async_sessionmaker(engine, expire_on_commit=False)
 
@@ -29,4 +32,14 @@ SessionDep = Annotated[AsyncSession, Depends(get_session)]
 
 
 class Base(DeclarativeBase):
-    pass
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+    )
