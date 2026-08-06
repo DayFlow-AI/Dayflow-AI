d9200382fda38c887594cf16e510d71baa11b8ae
Rostislav Nefedov <nefedov.r75@gmail.com>
2026-08-07 02:03:43 +0300

    add JWT model

--- user/models.py
+++ user/models.py
@@ -1,10 +1,25 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from db import Base
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from db_conf.db import Base
 
 
 class UsersORM(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str]
+    password: Mapped[str]
     email: Mapped[str]
+    jwt_tokens: Mapped[list["UsersJWTStorageORM"]] = relationship(
+        back_populates="user",
+        cascade="all, delete-orphan",
+    )
+
+class UsersJWTStorageORM(Base):
+    __tablename__ = "user_jwt_storage"
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id", onupdate="CASCADE", ondelete="CASCADE"))
+    username: Mapped[str]
+    device_type: Mapped[str | None] = mapped_column(default="undefined-device")
+    jwt_token: Mapped[str]
+    user : Mapped["UsersORM"] = relationship(back_populates="jwt_tokens")
