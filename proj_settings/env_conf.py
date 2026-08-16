bf2299564989f089868d1cb7fc181f9759fd8942
Rostislav Nefedov <nefedov.r75@gmail.com>
2026-08-16 12:19:04 +0300

    configure env with pydantic_settings

---
+++ proj_settings/env_conf.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+ENV_FILE = Path(__file__).resolve().parent / ".env"
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=ENV_FILE,
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
+
+    jwt_secret_key: str
+    jwt_secure: bool = False
+    client_id: str
+    client_sec: str
+    redirect_uri: str
+    db_pass: str
+    db_schema: str
+    db_user: str
+
+settings = Settings()
