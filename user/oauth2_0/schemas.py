8cabb49a05f665f2386915f2d2cfe3388ba387a2
Rostislav Nefedov <nefedov.r75@gmail.com>
2026-08-17 01:57:06 +0300

    add UserOauthInfo

--- user/oauth2_0/schemas.py
+++ user/oauth2_0/schemas.py
@@ -4,3 +4,9 @@ from pydantic import BaseModel, ConfigDict
 class OauthSchema(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     code: str
+
+
+class UserOauthInfo(BaseModel):
+    sub: str
+    email: str
+    name: str
