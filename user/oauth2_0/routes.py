c4c8c0b0b04a9c0fabf4532b53b40cddee7ba58d
Rostislav Nefedov <nefedov.r75@gmail.com>
2026-08-22 16:51:27 +0300

    fix OAuth (tested with fornt-end)

--- user/oauth2_0/routes.py
+++ user/oauth2_0/routes.py
@@ -1,4 +1,5 @@
-from authlib.oauth2 import OAuth2Error
+from datetime import datetime
+from authlib.integrations.base_client import OAuthError
 from fastapi import status, APIRouter, HTTPException, Response
 from pydantic import ValidationError
 from sqlalchemy import select
@@ -10,6 +11,7 @@ from user.models import UsersORM
 from user.oauth2_0.schemas import OauthSchema
 from user.oauth2_0.utils import get_google_profile
 from user.utils import gen_jwt, conf
+from fastapi.logger import logger
 
 oauth2_router = APIRouter(prefix="/api/oauth2", tags=["oauth2"])
 
@@ -17,7 +19,7 @@ client = AsyncOAuth2Client(
     client_id=settings.client_id,
     client_secret=settings.client_sec,
     redirect_uri=settings.redirect_uri,
-    scopes=settings.user_scopes,
+    scope=settings.user_scopes,
 )
 
 
@@ -29,7 +31,7 @@ async def auth_callback(code: OauthSchema, db: SessionDep) -> Response:
             code=code.code,
             grant_type="authorization_code",
         )
-        user_data = await get_google_profile(token["id_token"])
+        user_data = await get_google_profile(token["access_token"])
         query = insert(UsersORM).values(
             username=user_data.name,
             email=user_data.email,
@@ -40,21 +42,23 @@ async def auth_callback(code: OauthSchema, db: SessionDep) -> Response:
             index_elements=["google_sub"],
             set_={"username": user_data.name,
                   "email": user_data.email,
-                  "google_refresh_token": token["refresh_token"]},
+                  "google_refresh_token": token["refresh_token"],
+                  "updated_at": datetime.now()
+                  },
         ).returning(UsersORM.id)
         result = await db.execute(query)
         result = result.scalar_one()
-        user = select(UsersORM).where(UsersORM.id == result.id)
+        user = select(UsersORM).where(UsersORM.id == result)
         result = await db.execute(user)
         result = result.scalar_one()
         jwt = await gen_jwt(result, db)
         response = Response(status_code=status.HTTP_200_OK)
         response.set_cookie(**conf(jwt))
-        return response
 
-    except OAuth2Error as e:
+    except OAuthError as e:
         await db.rollback()
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=e)
+        logger.info("OAuthError info: %s", e.description)
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"error: OAuth authentication failed")
 
     except ValidationError as e:
         await db.rollback()
@@ -62,4 +66,8 @@ async def auth_callback(code: OauthSchema, db: SessionDep) -> Response:
 
     except Exception as e:
         await db.rollback()
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=e)
+        logger.error("Other mistakes: %s", e)
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Other mistakes")
+
+    else:
+        return response
--- user/oauth2_0/utils.py
+++ user/oauth2_0/utils.py
@@ -1,10 +1,12 @@
 from httpx import AsyncClient
-from joserfc import jwt, jwk
 from user.oauth2_0.schemas import UserOauthInfo
 
 
-async def get_google_profile(id_token: str) -> UserOauthInfo:
+async def get_google_profile(access_token: str) -> UserOauthInfo:
     async with AsyncClient() as c:
-        certs = (await c.get("https://www.googleapis.com/oauth2/v3/certs")).json()
-    claims = jwt.decode(id_token, jwk.KeySet(certs)).claims
-    return UserOauthInfo.model_validate(claims)
+        resp = await c.get(
+            "https://openidconnect.googleapis.com/v1/userinfo",
+            headers={"Authorization": f"Bearer {access_token}"},
+        )
+        data = resp.json()
+    return UserOauthInfo.model_validate(data)
