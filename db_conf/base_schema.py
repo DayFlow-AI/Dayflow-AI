00a19f5c343b0003a66a459907d2e1783b98b677
Rostislav Nefedov <nefedov.r75@gmail.com>
2026-08-22 16:49:58 +0300

    etc

--- db_conf/base_schema.py
+++ db_conf/base_schema.py
@@ -2,9 +2,8 @@ from pydantic import BaseModel
 
 
 class CustomSchema(BaseModel):
-    """База для входных схем: id назначает БД, клиент его не шлёт."""
+    ...
 
 
 class CustomOutSchema(CustomSchema):
-    """База для выходных схем: id отдаётся наружу."""
     id: int
