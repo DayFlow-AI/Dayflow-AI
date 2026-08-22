f8d22ad7148d4d295ff247ef88cc6c359efc42bf
Rostislav Nefedov <nefedov.r75@gmail.com>
2026-08-22 16:50:17 +0300

    connect ws into main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+from plan_board.ws import ws_router
 from user.routes import router as user_router
 from user.oauth2_0.routes import oauth2_router
 
@@ -20,6 +22,7 @@ app.add_middleware(
 
 app.include_router(user_router)
 app.include_router(oauth2_router)
+app.include_router(ws_router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True, host="172.30.88.250", port=8000)
